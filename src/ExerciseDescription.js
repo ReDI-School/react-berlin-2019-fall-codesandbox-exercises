1c84a0616cec84c504e4d293c7126d2419946516
Eric Bolikowski <eric@binarylights.com>
2019-09-09 16:14:46 +0200

    fix: capitalize <iframe> props correctly

--- src/ExerciseDescription.js
+++ src/ExerciseDescription.js
@@ -53,9 +53,9 @@ function ExerciseDescription({
               width="560"
               height="315"
               src="https://www.youtube.com/embed/9nw_ewL7n9o"
-              frameborder="0"
+              frameBorder="0"
               allow="autoplay; encrypted-media"
-              allowfullscreen
+              allowFullScreen
             />
             <div style={{ textAlign: 'left' }}>
               <p>
