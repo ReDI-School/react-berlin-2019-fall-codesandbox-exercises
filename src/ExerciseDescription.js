38cec6f447c1f020e45a0aded03b3e6ad748e69c
Christos Paschalidis <paschalidi@outlook.com>
2019-09-15 19:19:36 +0200

    Adds loops exercise (03)

--- src/ExerciseDescription.js
+++ src/ExerciseDescription.js
@@ -5,28 +5,29 @@ function ExerciseDescription() {
   const toggle = useCallback(() => setOpen(open => !open), []);
   return (
     <>
-      <h1>Kodiri > Beginner > Loop > Exercise 2</h1>
+      <h1>Kodiri > Beginner > Loop > Exercise 3</h1>
       <div>
         <button onClick={toggle}>
           {open ? 'Hide exercise details' : 'Open exercise details'}
         </button>
         {open && (
           <>
-            <h2>Display a list of actor details</h2>
-            <p>We have a list of actors, and we'd like to display their details:</p>
-            <p>name</p>
-            <p>age</p>
+            <h2>Display a list of Formula 1 World Champions</h2>
+            <p>
+              We have a large database of Formula 1 drivers, from legends like Ayrton Senna or Michael Schumacher to some others like Luca Badoer or Alex Yoong.
+              We'd like to display a list of World Champions!
+            </p>
             <iframe
               width="560"
               height="315"
-              src="https://youtu.be/TvEC7A7IoMU"
+              src="https://youtu.be/CzacQpUKLFI"
               frameborder="0"
               allow="autoplay; encrypted-media"
               allowfullscreen
             />
             <div style={{ textAlign: 'left' }}>
               <p>
-                Edit <strong>Actors.js</strong> to complete the exercise.
+                Edit <strong>WorldChampions.js</strong> to complete the exercise.
               </p>
               <p>Switch to the "Tests" tab to check your results.</p>
               <p>
@@ -34,7 +35,7 @@ function ExerciseDescription() {
                 in Slack).
               </p>
               <p>
-                The tests are defined in Actors.spec.js. You do{' '}
+                The tests are defined in WorldChampions.spec.js. You do{' '}
                 <strong>not need</strong> to understand in detail how unit tests
                 work, but feel free to look in the file if you're curious.
               </p>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import ExerciseDescription from "./ExerciseDescription";
-import Actors from "./Actors";
+import Actors from "./WorldChampions";
 
 import "./styles.css";
 
