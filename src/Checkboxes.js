6578eb04097cd6a61ca6e1e12c51481b9dcacc40
Christos Paschalidis <paschalidi@outlook.com>
2019-09-17 20:43:01 +0200

    adds template

--- src/Checkboxes.js
+++ src/Checkboxes.js
@@ -3,12 +3,16 @@ import React from "react";
 function Checkboxes() {
 	return (
 		<div>
-			<label>
-				Accepts the terms and conditions.
-			</label>
-			<label>
-				Accepts to receive a monthly newsletter.
-			</label>
+			<div>
+				<label>
+					Accepts the terms and conditions.
+				</label>
+			</div>
+			<div>
+				<label>
+					Accepts to receive a monthly newsletter.
+				</label>
+			</div>
 		</div>
 	);
 }
