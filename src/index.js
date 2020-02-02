e77ec129ff3dee5a64a5af463c174b091991ae9c
Christos Paschalidis <paschalidi@outlook.com>
2020-02-02 22:17:06 +0100

    styles

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import ExerciseDescription from './ExerciseDescription';
+import {ExerciseDescription, SolutionArea, conditionalRendering, GlobalStyles} from 'react-exercises-ui'
 import FilmScore from './FilmScore';
-
-import './styles.css';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <ExerciseDescription
-          level="Beginner"
-          topic="Conditional Rendering"
-          exerciseNumber="4"
-          title="How good a film is?"
-          exerciseComponentName="FilmScore"
-        />
-      </div>
-
-      <hr />
-      <FilmScore title="The Fountain" score={90} />
+      <GlobalStyles/>
+      <ExerciseDescription {...conditionalRendering.ex04} />
+      <SolutionArea component={<FilmScore title="The Fountain" score={90} />} />
     </>
   );
 }
