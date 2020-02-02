8f23cba4eaaaf7d02e009a54f53e0e124cd28876
Christos Paschalidis <paschalidi@outlook.com>
2020-02-02 20:58:01 +0100

    adds new lib

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import ExerciseDescription from './ExerciseDescription';
+import {ExerciseDescription, SolutionArea, conditionalRendering, GlobalStyles} from 'react-exercises-ui'
 import Welcome from './Welcome';
-
-import './styles.css';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <ExerciseDescription
-          level="Beginner"
-          topic="Conditional Rendering"
-          exerciseNumber="1"
-          title="Welcome users"
-          exerciseComponentName="Welcome"
-        />
-      </div>
-
-      <hr />
-      <Welcome firstName="Valentino" lastName="Rossi" />
+      <GlobalStyles />
+      <ExerciseDescription{...conditionalRendering.ex01} />
+      <SolutionArea component={<Welcome firstName="Valentino" lastName="Rossi" />}/>
     </>
   );
 }
